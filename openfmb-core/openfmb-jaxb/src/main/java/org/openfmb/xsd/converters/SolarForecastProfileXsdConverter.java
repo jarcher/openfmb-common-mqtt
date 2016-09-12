a52b1f2704ccb09462860f96bed08f632a0a79ab
Dwayne Bradley <dwayne.bradley@duke-energy.com>
2016-09-12 15:24:32 -0400

    New copyright info

--- openfmb-core/openfmb-jaxb/src/main/java/org/openfmb/xsd/converters/SolarForecastProfileXsdConverter.java
+++ openfmb-core/openfmb-jaxb/src/main/java/org/openfmb/xsd/converters/SolarForecastProfileXsdConverter.java
@@ -1,13 +1,15 @@
-/*
- * Licensed to Duke Energy Corporation (www.duke-energy.com) under one or more
+/**
+ * Copyright 2016 Duke Energy.
+ *
+ * Licensed to Duke Energy (www.duke-energy.com) under one or more
  * contributor license agreements. See the NOTICE file distributed with this
  * work for additional information regarding copyright ownership. Duke Energy
- * Corporation licenses this file to you under the Apache License, Version 2.0 (the
+ * licenses this file to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
--- openfmb-core/openfmb-jaxb/src/main/java/org/openfmb/xsd/converters/gson/XMLGregorianCalendarConverter.java
+++ openfmb-core/openfmb-jaxb/src/main/java/org/openfmb/xsd/converters/gson/XMLGregorianCalendarConverter.java
@@ -1,3 +1,21 @@
+/**
+ * Copyright 2016 Duke Energy.
+ *
+ * Licensed to Duke Energy (www.duke-energy.com) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. Duke Energy
+ * licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package org.openfmb.xsd.converters.gson;
 
 import java.lang.reflect.Type;
@@ -22,7 +40,6 @@ public class XMLGregorianCalendarConverter
 {
     public static class Serializer implements JsonSerializer<XMLGregorianCalendar>
     {
-        @Override
         public JsonElement serialize(XMLGregorianCalendar xmlGregorianCalendar, Type type, JsonSerializationContext jsonSerializationContext)
         {
             return new JsonPrimitive(xmlGregorianCalendar.toXMLFormat());
@@ -31,7 +48,6 @@ public class XMLGregorianCalendarConverter
 
     public static class Deserializer implements JsonDeserializer<XMLGregorianCalendar>
     {
-        @Override
         public XMLGregorianCalendar deserialize(JsonElement jsonElement, Type type,
                 JsonDeserializationContext jsonDeserializationContext)
         {
